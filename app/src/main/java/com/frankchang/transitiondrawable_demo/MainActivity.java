1ebf809191773d81d04a54e9165854e3d419b38f
lus33frank <lus33frank@gmail.com>
2018-10-24 00:08:54 +0800

    Commit : 2    專案練習完成。 圖片漸變動畫

--- app/src/main/java/com/frankchang/transitiondrawable_demo/MainActivity.java
+++ app/src/main/java/com/frankchang/transitiondrawable_demo/MainActivity.java
@@ -1,13 +1,30 @@
 package com.frankchang.transitiondrawable_demo;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TransitionDrawable transition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgShow = findViewById(R.id.imgShow);
+        transition = (TransitionDrawable) getResources()
+                .getDrawable(R.drawable.photo, null);
+        imgShow.setImageDrawable(transition);
     }
+
+    public void loadingOnClick(View view) {
+        transition.startTransition(6000);
+    }
+
 }
